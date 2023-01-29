56d7bf8808d06ac47545f9afb36c47a83f38e44c
Mohammad Abu Musa <m.abumusa@gmail.com>
2023-01-29 09:43:57 +0000

    inital dashboard design

--- app/Filament/Pages/MyProfile.php
+++ app/Filament/Pages/MyProfile.php
@@ -4,17 +4,32 @@ namespace App\Filament\Pages;
 
 use JeffGreco13\FilamentBreezy\Pages\MyProfile as BaseProfile;
 use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Grid;
 
 class MyProfile extends BaseProfile
 {
 
    protected function getUpdateProfileFormSchema(): array
     {
-        return array_merge(parent::getUpdateProfileFormSchema(), [
-            Forms\Components\TextInput::make("job_title"),
-            Forms\Components\Checkbox::make("marketing_consent")->label(
-                "I consent to receive email notifications....."
-            ),
-        ]);
+        return [
+            Grid::make()
+            ->columns(2)
+            ->schema([
+                TextInput::make('first_name')
+                    ->label(__('app.first_name'))
+                    ->required(),
+                TextInput::make('last_name')
+                    ->label(__('app.last_name'))
+                    ->required(),
+            ]),
+
+
+            TextInput::make($this->loginColumn)
+                ->required()
+                ->email(fn () => $this->loginColumn === 'email')
+                ->unique(config('filament-breezy.user_model'), ignorable: $this->user)
+                ->label(__('filament-breezy::default.fields.email')),
+        ];
     }
 }
